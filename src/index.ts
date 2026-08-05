382a5982309783cee3b44b06aee0f24538945bc5
Justin Szaro <justinszaro@gmail.com>
2026-08-04 22:13:44 -0400

    feat: create players with custom strategies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import Player from './models/core/Player';
 import Game from './models/Game';
+import RandomRicky from './models/players/RandomRicky';
+import PurplePepper from './models/players/PurplePepper';
+import GreenGatsby from './models/players/GreenGatsby';
+import BlueBen from './models/players/BlueBen';
+import RedRicky from './models/players/RedRicky';
 
 export {
   Player,
-  Game
+  Game,
+  RandomRicky,
+  PurplePepper,
+  GreenGatsby,
+  BlueBen,
+  RedRicky,
 }
